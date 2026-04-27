616c33593009b5b76085cbac4aaac0678aa15942
noone <boringmkt@gmail.com>
2026-04-26 22:57:24 -0300

    startedc02ex10

---
+++ 02_c/C02/ex10/ft_strlcpy.c
@@ -0,0 +1,11 @@
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	
+}
+
+/*
+• Reproduzir de forma idêntica o funcionamento da função strlcpy (man strlcpy).
+
+• Ela deverá ser prototipada da seguinte maneira:
+	unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
+*/
